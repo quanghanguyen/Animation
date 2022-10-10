732f711cba9c3de327759220d0a5352320d7fb9e
QuangHa <duongthien.mcs@gmail.com>
2022-10-10 09:41:14 +0700

    1010

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    let animationView = AnimationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupAnimation()
     }
 
+    private func setupAnimation() {
+        animationView.animation = Animation.named("animation")
+        animationView.frame = view.bounds
+        animationView.backgroundColor = .white
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
+    }
 
 }
 
